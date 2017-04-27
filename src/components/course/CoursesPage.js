631ff915e66015bdb17744847b400214f2cf7db9
Maks <andreev171293@gmail.com>
2017-04-27 22:28:29 +0300

    Connect redux

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,11 +1,68 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {createCourse} from '../../actions/courseActions.js'
 
 class CoursesPage extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      course: {title: ""}
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onInputSubmit = this.onInputSubmit.bind(this);
+  }
+  onInputChange(event){
+    const title = event.target.value;
+    this.setState({
+      course: {title: title}
+    })
+  }
+  onInputSubmit(){
+    this.props.createCourse(this.state.course);
+    console.log(this.props.courses);
+  }
   render(){
+    const showCourse = (course, index) => {
+      return (
+        <div key={index}>
+        {course.title}
+        </div>
+      )
+    }
     return (
-      <h1>Courses</h1>
-    )
+      <div>
+        <h1>Courses</h1>
+        {this.props.courses.map(showCourse)}
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          defaultValue={this.state.course.title}
+          placeholder="Enter the title of the course"
+          onChange={this.onInputChange}
+        />
+        <input
+          type="submit"
+          defaultValue="Save"
+          onClick={this.onInputSubmit}
+        />
+      </div>
+    );
+  }
+}
+CoursesPage.propTypes = {
+  createCourse: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired
+}
+function mapStateToProps(state){
+  return {
+    courses: state.courses
+  }
+}
+function mapDispatchToProps(dispatch){
+  return{
+    createCourse: bindActionCreators(createCourse, dispatch)
   }
 }
 
-export default CoursesPage;
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory, IndexRoute} from 'react-router';
+import {Provider} from 'react-redux';
+import configureStore from './store/configureStore';
 import AboutPage from './components/about/AboutPage';
 import HomePage from './components/home/HomePage';
 import CoursesPage from './components/course/CoursesPage';
@@ -9,12 +11,16 @@ import App from './components/App';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path='/' component={App}>
-      <IndexRoute component={HomePage} />
-      <Route path='about' component={AboutPage} />
-      <Route path='courses' component={CoursesPage} />
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <IndexRoute component={HomePage} />
+        <Route path='about' component={AboutPage} />
+        <Route path='courses' component={CoursesPage} />
+      </Route>
+    </Router>
+  </Provider>
 , document.getElementById('app'));
